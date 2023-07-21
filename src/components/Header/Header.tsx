3bdc43a606d83a628e955b2aef2853d65cb79dda
RomanSur1998 <123935997+RomanSur1998@users.noreply.github.com>
2023-07-21 11:35:31 +0600

    added header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/logo.svg";
+import search from "../../assets/circum_search.svg";
 
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div className={styles.header_nav}>
+        <h5>Главная </h5>
+        <h5>Курсы </h5>
+        <h5>Расписание</h5>
+        <h5>Регистрация </h5>
+      </div>
+      <div className={styles.header_search}>
+        <h5>Рус</h5>
+        <img src={search} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
